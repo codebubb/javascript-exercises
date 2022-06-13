ef551c887f86c986f7ba5bbb4657ac443a5cd807
James Bubb <jpbubb82@gmail.com>
2022-06-13 22:02:00 +0100

    refactor: tidy ex1 spec

--- tests/exercise-01.spec.js
+++ tests/exercise-01.spec.js
@@ -5,17 +5,14 @@ Write a JavaScript program to check two numbers and return true if
 one of the numbers is 100 or if the sum of the two numbers is 100
 `, () => {
   it('should return true if the first number is 100', () => {
-    const result = isEqualTo100(100, 0);
-    expect(result).toBe(true);
+    expect(isEqualTo100(100, 0)).toBe(true);
   });
 
   it('should return true if the second number is 100', () => {
-    const result = isEqualTo100(0, 100);
-    expect(result).toBe(true);
+    expect(isEqualTo100(0, 100)).toBe(true);
   });
 
   it('should return true if the sum of the numbers is 100', () => {
-    const result = isEqualTo100(50, 50);
-    expect(result).toBe(true);
+    expect(isEqualTo100(50, 50)).toBe(true);
   });
 });
